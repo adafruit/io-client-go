1e0caae4d1062ecb8d293f4524ec00fa8d54784a
Adam Bachman <adam.bachman@gmail.com>
2016-05-24 16:28:47 -0400

    add -feed and -value to send_data example

--- examples/send_data.go
+++ examples/send_data.go
@@ -8,22 +8,28 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"net/url"
 
 	"github.com/adafruit/io-client-go"
 )
 
 func main() {
 	var key string
+	var feed string
+	var value string
+
 	flag.StringVar(&key, "key", "", "your Adafruit IO key")
+	flag.StringVar(&feed, "feed", "beta-test", "the feed to send to")
+	flag.StringVar(&value, "value", "42", "the value to send")
+
 	flag.Parse()
 
 	client := adafruitio.NewClient(key)
-	client.BaseURL, _ = url.Parse("http://localhost:3002/")
+	// client.BaseURL, _ = url.Parse("http://localhost:3002/")
 
 	// create a data point on an existing Feed
-	client.SetFeed("beta-test")
-	val := json.Number("22")
+	client.SetFeed(feed)
+	val := json.Number(value)
+
 	dp, _, err := client.Data.Send(&adafruitio.DataPoint{Value: &val})
 	if err != nil {
 		fmt.Println("UNEXPECTED ERROR!", err)
